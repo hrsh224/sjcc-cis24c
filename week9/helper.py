1f5a954a5d23b018d3362aa56bcbcf87474ad676
Harish Allamraju <hari.allamraju@outlook.com>
2024-06-13 21:51:45 -0700

    pushing lab and hw 10

--- week9/helper.py
+++ week9/helper.py
@@ -1,7 +1,22 @@
+"""
+def print_primes(start, end):
+    for num in range(start, end + 1):
+        if num > 1:  # prime numbers are greater than 1
+            for i in range(2, num):
+                if (num % i) == 0:  # if the number is divisible by any number between 2 and itself, it's not prime
+                    break
+            else:
+                print(num)
 
-### YOUR CODE to set up the helper function GOES BELOW
-def add(x, y):
-    return x + y
+# Example usage:
+print_primes(10, 20)
+"""
+def print_square(number):
+    square = number ** 2
+    print(f"The square of {number} is {square}")
 
+# Get the number from the user
+num = int(input("Enter a number: "))
 
-### END CODE
+# Call the function
+print_square(num)
